ce12e714770c245c920ee55910b175b917bc3b0e
Lucas Almeida <lucasaclima03@gmail.com>
2022-01-28 11:51:14 -0300

    initial structure

---
+++ lib/service/dio_service_imp.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:dio/src/dio.dart';
+import 'package:movies/service/dio_service.dart';
+
+class DioServiceImp implements DioService {
+  @override
+  Dio getDio() {
+    return Dio(
+      BaseOptions(
+        baseUrl: 'https://api.themoviedb.org/4/',
+        headers: {
+          'content-type': 'application/json;charset=utf-8',
+          'authorization':
+              'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzhmMWUxODk1NGM2MzllZjBhMGViODAyMTg3NmI2ZiIsInN1YiI6IjYxNDM0NDRiNTZiOWY3MDA0NTAwZWY3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yTzB3-3NJ_jon7jHZ5fqVcHrgCT5rUrfuMyrezqSz9Q'
+        },
+      ),
+    );
+  }
+}
